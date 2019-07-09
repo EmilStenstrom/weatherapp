a0df1aff5a69bb863e35d6f6c3f76d90c2421872
Emil Stenström <em@kth.se>
2019-07-09 12:52:35 +0200

    Check if data is younger than 1 hour before fetching.

--- refresh.py
+++ refresh.py
@@ -1,8 +1,12 @@
 import json
 import os
 import re
+import sys
 import urllib.parse
 import urllib.request
+from datetime import datetime
+
+import pytz
 
 
 def httpget(url):
@@ -22,12 +26,37 @@ def env_or_error(key, _format, default=None):
 
     return value
 
+def check_existing_file(path):
+    if not os.path.isfile(path):
+        return
+
+    with open(path, "r") as f:
+        data = json.load(f)
+
+    # Get local time in UTC
+    local_dt = datetime.now(pytz.utc)
+
+    # Get localized date from response
+    response_time = datetime.fromtimestamp(data["currently"]["time"])
+    response_tz = pytz.timezone(data["timezone"])
+    response_dt = response_tz.localize(response_time)
+
+    # Was the data fetched less than one hour ago?
+    hours_diff = (local_dt - response_dt).total_seconds() / (60*60)
+    if hours_diff < 1:
+        print("Will not fetch, data is less than 1 hour old")
+        sys.exit()
+
+    return
+
 def main():
     apikey = env_or_error("APIKEY", r"[a-f0-9]+")
     location = env_or_error("LOCATION", r"^\-?\d+\.\d+,\-?\d+\.\d+?$", default="59.3078312,18.0075784")
     lang = env_or_error("LANGAUGE", r"[a-z]{2}", default="sv")
 
     weather_path = "weather/weather.json"
+    check_existing_file(weather_path)
+
     with open(weather_path, "w") as f:
         url = f"https://api.darksky.net/forecast/{apikey}/{location}/?lang={lang}&units=si"
         weather = httpget(url)
